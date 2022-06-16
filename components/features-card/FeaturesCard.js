4477fe236c0067f5d8edacd3b889644a1cb80062
Amin_Cs <aminnoor.cs@gmail.com>
2022-06-16 18:35:41 +0500

    origin14d

--- components/features-card/FeaturesCard.js
+++ components/features-card/FeaturesCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./FeaturesCard.module.css";
-import ImgLazyLoad from "../img-lazy-load/Img-lazy-load";
+import ImgLazyLoad from "../img-lazy-load/ImgLazyLoad";
 
 function FeaturesCard({ data }) {
   return (
